eae9af88bc10fa74a523a8f9d641f51ec2f3e532
王博 <101862153+Bgehaogao@users.noreply.github.com>
2024-08-21 19:20:20 +0800

    Add files via upload

---
+++ W1D3/main.py
@@ -0,0 +1,43 @@
+def add(x, y):  # 加法
+    return x + y
+
+
+def minus(x, y):  # 减法
+    return x - y
+
+
+def multiply(x, y):  # 乘法
+    return x * y
+
+
+def divide(x, y):  # 除法
+    if y == 0:
+        return "错误"  # 保证除数不为零
+    return x / y
+
+
+def calculator():  # 计算器主函数
+
+    choice = input("请输入你的选择(add/minus/multiply/divide): ")
+
+    if choice in ["add", "minus", "multiply", "divide"]:
+        num1 = float(input("请输入第一个数字: "))
+        num2 = float(input("请输入第二个数字: "))
+
+        if choice == "add":
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+
+        elif choice == "minus":
+            print(f"{num1} - {num2} = {minus(num1, num2)}")
+
+        elif choice == "multiply":
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+        elif choice == "divide":
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
+    else:
+        print("无效输入")
+
+
+# 运行计算器
+calculator()
